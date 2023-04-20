5edd6dc4438f6ab48cfe94647463cf9e155f7291
Navneeth Ramachandran <101825015+navneethramachandran14@users.noreply.github.com>
2023-04-20 14:08:37 +0530

    Update COVIDPortfolioProject.sql

--- COVIDPortfolioProject.sql
+++ COVIDPortfolioProject.sql
@@ -142,16 +142,3 @@ From #PercentPopulationVaccinated
 
 
 
-
--- Creating View to store data for later visualizations
-
-Create View PercentPopulationVaccinated as
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
-
